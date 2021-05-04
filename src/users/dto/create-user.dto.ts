f97394e21da5811feebac5487d7b8d8a980a3328
Letícia Cellurale <leticia.cellurale@gmail.com>
2021-05-03 22:18:12 -0300

    :heavy_plus_sign: add typem and docker with postgres

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,6 @@
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class CreateUserDto {
-  @IsEmail()
-  email: string;
-
   @IsNotEmpty()
-  password: string;
+  name: string;
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const newUser = await this.usersRepository.save({ ...createUserDto });
+    return newUser;
   }
 
   findAll() {
